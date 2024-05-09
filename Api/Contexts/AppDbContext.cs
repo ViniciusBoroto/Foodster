ce393fbbd2b9274e7e4bd01a8d34f15378dcb12b
ViniciusBoroto <viniciusdosanjos63@gmail.com>
2024-05-09 20:23:47 -0300

    API creation

---
+++ Api/Contexts/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    DbSet<Item> Items { get; set; }
+}
---
+++ Api/Repositories/IItemsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Api.Models;
+
+namespace Api.Repositories;
+
+public interface IItemsRepository
+{
+    public IEnumerable<Item> GetItems();
+    public Item Get(int id);
+    public Item Create(Item item);
+    public Item Update(Item item);
+    public Item Delete(int id);
+}
